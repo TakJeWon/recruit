ecb9fde4d231fab5c32ee0bdfe5c317a766daeb3
taknineball <4321she!>
2023-02-14 20:07:38 +0900

    ㅅadmin페이지에는 로그인 해야만 접근할 수 있도록 변경 (로그인 후 로그인 페이지 접속 시 처리 해야함, 권한에 따라 static파일들 정리해야함_)

---
+++ src/main/java/com/castis/career/controller/AdminController.java
@@ -0,0 +1,158 @@
+package com.castis.career.controller;
+
+import com.castis.career.entity.Apply;
+import com.castis.career.entity.Board;
+import com.castis.career.entity.Member;
+import com.castis.career.service.ApplyService;
+import com.castis.career.service.BoardService;
+import com.castis.career.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping("/")
+    public String getLoginForm(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(auth instanceof AnonymousAuthenticationToken)){
+
+            //로그인성공
+            return "redirect:/admin/dashboard";
+        }
+        return "redirect:/admin/adminLogin";
+    }
+
+    @GetMapping("/adminLogin")
+    public String adminLogin(){
+        return "/adminView/adminLogin";
+    }
+
+    @GetMapping("/dashboard")
+    public String adminPage(){
+        return "/adminView/adminDashboard";
+    }
+
+    @GetMapping("/boardSetting")
+    public String adminBoardSetting(Model model){
+
+        model.addAttribute("boardList", boardService.boardList());
+
+        return "/adminView/adminBoardSetting";
+    }
+
+    @GetMapping("/register")
+    public String adminAddBoard(){
+        return "/adminView/adminAddBoard";
+    }
+
+    @PostMapping("/register/success")
+    public String adminAddBoardSuccess(Board board, MultipartFile file) throws IOException {
+
+        boardService.write(board, file);
+
+        return "redirect:/boardSetting";
+    }
+
+    @GetMapping("/register/delete")
+    public String adminBoardDelete(Integer id){
+        boardService.boardDelete(id);
+
+        return "redirect:/boardSetting";
+    }
+
+    @GetMapping("/boardView")
+    public String adminBoardView( Integer id, Model model){
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "/adminView/adminBoardView";
+    }
+
+    @PostMapping("/register/update/{id}")
+    public String adminBoardUpdate(@PathVariable("id") Integer id,
+                                   Board board,
+                                   MultipartFile file) throws IOException {
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        boardTemp.setEducation(board.getEducation());
+        boardTemp.setExperience(board.getExperience());
+        boardTemp.setType(board.getType());
+        boardTemp.setLocation(board.getLocation());
+        boardTemp.setStart_date(board.getStart_date());
+        boardTemp.setEnd_date(board.getEnd_date());
+        boardTemp.setFilename(board.getFilename());
+        boardTemp.setFilepath(board.getFilepath());
+
+        boardService.write(boardTemp, file);
+
+        return "redirect:/boardSetting";
+    }
+
+    // 지원서 ; 첨부 파일 다운로드
+    @GetMapping("/applyDownload/{id}")
+    public ResponseEntity<UrlResource> applyDownloadAttach(@PathVariable Integer id) throws MalformedURLException {
+
+        Apply apply_file = applyService.applyView(id);
+        String apply_fileName = apply_file.getFilename();
+        String apply_orgName = apply_fileName.substring(apply_fileName.lastIndexOf("."));
+
+        UrlResource resource = new UrlResource("file:" + apply_file.getFilepath());
+        String encodedFileName = UriUtils.encode(apply_fileName, StandardCharsets.UTF_8);
+
+        // 파일 다운로드 대화상자가 뜨도록 하는 헤더를 설정해주는 것
+        // Content-Disposition 헤더에 attachment; filename="업로드 파일명" 값을 준다.
+        String contentDisposition = "attachment; filename=\"" + encodedFileName + "\"";
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,contentDisposition).body(resource);
+    }
+
+    @GetMapping("/memberList")
+    public String memberListView(Model model){
+
+        model.addAttribute("memberList", memberService.MemberList());
+
+        return "/adminView/adminMemberList";
+    }
+
+    @GetMapping("/addMember")
+    public String addMemberView(){
+        return "/adminView/adminAddMember";
+    }
+
+    @PostMapping("/member/success")
+    public String createMember(Member member){
+
+        memberService.save(member);
+        return "redirect:/admin/memberList";
+    }
+
+}
