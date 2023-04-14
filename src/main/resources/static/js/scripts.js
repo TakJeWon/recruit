04226eb90a9b6ad6bf56d8f81ad8210d7e48555c
taknineball <4321she!>
2023-04-14 19:03:23 +0900

    HOW WE WORK 부분 내용 수정

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -3,6 +3,12 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
+
+// core version + navigation, pagination modules:
+
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
